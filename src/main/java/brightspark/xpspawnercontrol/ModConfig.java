ebd38a737634f96a5bf5a16c4703110a4ec01206
bright_spark <markpassey@btinternet.com>
2018-04-05 13:20:32 +0100

    Removed configs requiring a restart

--- src/main/java/brightspark/xpspawnercontrol/ModConfig.java
+++ src/main/java/brightspark/xpspawnercontrol/ModConfig.java
@@ -7,11 +7,9 @@ import net.minecraftforge.common.config.Config;
 public class ModConfig
 {
     @Config.Comment("Whether the entity list is a blacklist (true) or a whitelist (false) of removing experience drops")
-    @Config.RequiresMcRestart
     public static boolean isBlacklist = true;
 
     @Config.Comment("A list of all of the entity registry names which this mod will affect (e.g. minecraft:zombie)")
-    @Config.RequiresMcRestart
     public static String[] entityList = {""};
 
     @Config.Comment("Set to true to turn on debugging log messages")
